f05099742a6af3ecc7d4ad72c1f90e85cb63b767
lee98064 <lee98064@gmail.com>
2022-01-21 20:43:19 +0800

    git init

---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app style="background-color: #EEEEEE">
+    <v-main>
+      <SideBar />
+      <Nuxt />
+      <SideBarRight />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import SideBar from "~/components/SideBar.vue"
+
+export default {
+  components: {SideBar},
+  name: 'DefaultLayout',
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
